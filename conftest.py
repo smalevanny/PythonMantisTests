0914d832b7c8a40105d456884a50081e41b03790
Sergei Malevannyi <sergey.malevanny@gmail.com>
2022-12-02 10:20:16 +0200

    first test

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import json
+import os.path
+
+import pytest
+
+from fixture.application import Application
+
+fixture = None
+target = None
+
+def load_config(file):
+    global target
+    if target is None:
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_path) as config_file:
+            target = json.load(config_file)
+    return target
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))["web"]
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config["baseURL"])
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--target", action="store", default="target.json")
+
+
+
+
---
+++ fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        else:
+            raise ValueError(f"Unrecognized browser = {browser}")
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "home page").click()
+
+    def destroy(self):
+        self.wd.quit()
